d45ec2e846d32f316c5835331ee248cb4958e3c4
Ahmir Robinaon <ahmirrobinson981@gmail.com>
2025-05-07 21:21:55 -0400

    first commit

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress';
+import viteConfig from './vite.config';
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+      viteConfig,
+    },
+    
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+});
